aabcc974f6be61e6aa5613a912b4f08ade8e3645
APA\SESA426602 <Jasmine.Kaur@schneider-electric.com>
2016-11-10 15:23:00 +1100

    c# files

---
+++ JasmineWebApi-Asp.net/ProductsApp/ProductsApp/Models/SearchInformation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductsApp.Models
+{
+    public class SearchInformation
+    {
+        public string Label
+        {
+            get;
+            set;
+        }
+        public string Url
+        {
+            get;
+            set;
+        }
+    }
+}
